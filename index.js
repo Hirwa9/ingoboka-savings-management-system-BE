1d9d91eaaabbca1b1e1c4714ea0852e5dbb322f3
hirwa9 <hirwawilly9@gmail.com>
2025-02-08 00:33:42 +0200

    Use render URLs in cors origin

--- index.js
+++ index.js
@@ -37,28 +37,6 @@ app.use(cors({
     }
 }));
 
-// app.use(cors({
-//     credentials: true,
-//     origin: allowedOrigins
-// }));
-
-// app.use((req, res, next) => {
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//         res.setHeader("Access-Control-Allow-Origin", origin); // Set origin dynamically
-//     }
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-//     // Handle preflight requests
-//     if (req.method === "OPTIONS") {
-//         return res.sendStatus(200);
-//     }
-
-//     next();
-// });
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
