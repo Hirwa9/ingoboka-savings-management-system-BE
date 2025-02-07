b255786989c17e855cdc60499307801e3bcb58b4
hirwa9 <hirwawilly9@gmail.com>
2025-02-07 14:25:40 +0200

    Use Aiven hosted database credentials

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,28 @@
+// Local
+
+// import { Sequelize } from "sequelize";
+
+// const db = new Sequelize('ingoboka_sm_system', 'root', '', {
+//     host: "localhost",
+//     dialect: "mysql"
+// });
+
+// export default db;
+
+// Hosted
+
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('ingoboka_sm_system', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
+const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_ROOT_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default db;
