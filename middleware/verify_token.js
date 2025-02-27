1aff28f984b88730b6c32c9c9540e24cf511ba9b
hirwa9 <hirwawilly9@gmail.com>
2025-02-27 12:12:31 +0200

    Refine token verification

--- middleware/verify_token.js
+++ middleware/verify_token.js
@@ -1,12 +1,17 @@
 import jwt from "jsonwebtoken";
 
-export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) return res.sendStatus(403);
-        req.email = decoded.email;
-        next();
-    })
-}
+export const verifyToken = (req, res) => {
+    const accessToken = req.cookies.accessToken;
+    console.log('_________ reached')
+    if (!accessToken) {
+        return res.status(401).json({ message: "Access token missing" });
+    }
+    
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid or expired token" });
+        }
+        console.log('_________ Verified user', user)
+        res.json({ user });
+    });
+};
