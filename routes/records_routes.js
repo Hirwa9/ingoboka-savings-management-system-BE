9cb7c84d0533cab2cf423e4769004adb0417e984
hirwa9 <hirwawilly9@gmail.com>
2025-03-25 18:44:27 +0200

    Add expense record edit and delete routes

--- routes/records_routes.js
+++ routes/records_routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { addCreditPenalty, addExpense, getRecords } from '../controllers/records.js';
+import { addCreditPenalty, addExpense, deleteExpense, editExpense, getRecords } from '../controllers/records.js';
 
 const recordsRouter = express.Router();
 
-recordsRouter.get('/records', getRecords);                         // Get records
+recordsRouter.get('/records', getRecords);                          // Get records
 recordsRouter.post('/records/recordExpense', addExpense);           // Add expense record
-recordsRouter.post('/user/:id/credit-penalty', addCreditPenalty);
+recordsRouter.post('/record/edit', editExpense);                    // Edit expense record
+recordsRouter.post('/record/delete', deleteExpense);                // Delete expense record
+recordsRouter.post('/user/:id/credit-penalty', addCreditPenalty);   // Create penalty record
 
 export default recordsRouter;
