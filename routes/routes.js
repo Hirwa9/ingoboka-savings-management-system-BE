347bb362e0ac513bf001b10eca7eee2672bdcf3f
hirwa9 <hirwawilly9@gmail.com>
2025-03-29 13:58:06 +0200

    Use loans router

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ import figuresRouter from "./figures_routes.js";
 import creditsRouter from "./credits_routes.js";
 import recordsRouter from "./records_routes.js";
 import usersRouter from "./users_routes.js";
+import loansRouter from "./loan_routes.js";
 import annualInterestsRouter from "./annual_interests_routes.js";
 
 const router = express.Router();
@@ -14,8 +15,9 @@ router.use(figuresRouter);                              // Figures routes
 router.use(usersRouter);                                // Users routes
 router.use(recordsRouter);                              // Records routes
 router.use(creditsRouter);                              // Credits routes
+router.use(loansRouter);                                // Loans routes
 router.use(annualInterestsRouter);                      // Annual interests routes
-router.use('/api/settings', settingsRouter);                // ettings routes
+router.use('/api/settings', settingsRouter);            // Settings routes
 
 router.post('/api/database/backup', backupDatabase);    // Backup the database
 
